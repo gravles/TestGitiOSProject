01af9c71c676fa14dc198468e58a67315b67ebec
Nathan Davie <nathandavie@easymanifest.com>
2014-03-24 13:38:09 -0400

    second commit

---
+++ EMtest.h
@@ -0,0 +1,13 @@
+//
+//  EMtest.h
+//  GitTest
+//
+//  Created by Nathan Davie on 2014-03-24.
+//  Copyright (c) 2014 Easy Manifest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EMtest : NSObject
+
+@end
---
+++ EMtest.m
@@ -0,0 +1,13 @@
+//
+//  EMtest.m
+//  GitTest
+//
+//  Created by Nathan Davie on 2014-03-24.
+//  Copyright (c) 2014 Easy Manifest. All rights reserved.
+//
+
+#import "EMtest.h"
+
+@implementation EMtest
+
+@end
